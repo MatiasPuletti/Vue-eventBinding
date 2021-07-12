976bed04ca737eb851cf628a94165132ea7dbf53
matiaspuletti <pulettimatias@gmail.com>
2021-07-12 11:12:22 +0200

    Register user input on keydown and output in the paragraph

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const app = Vue.createApp({
   data() {
     return {
-    
+      userInput: ''
     };
   },
   methods: {
-      clickButton(event) {
-          alert('You just clicked the button!')
-      }
+    clickButton(event) {
+      alert('You just clicked the button!');
+    },
+    keydownInput(event) {
+      this.userInput = event.target.value;
+    }
   }
 });
 
